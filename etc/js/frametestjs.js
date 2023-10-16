eb2e8e558445059fe2a46544f7881d8c55856a8b
donuchew <120776338+donuchew@users.noreply.github.com>
2023-10-16 16:23:06 +0900

    Update frametestjs.js

--- etc/js/frametestjs.js
+++ etc/js/frametestjs.js
@@ -1,34 +1,30 @@
-body {
-    height: 300vh; /* 충분한 높이를 갖는 페이지 */
-    margin: 0;
-    overflow: hidden;
+let currentFrame = 1;
+
+window.addEventListener('wheel', (event) => {
+    if (event.deltaY > 0) {
+        // 스크롤 다운: 다음 프레임으로 이동
+        currentFrame = Math.min(currentFrame + 1, 3);
+    } else {
+        // 스크롤 업: 이전 프레임으로 이동
+        currentFrame = Math.max(currentFrame - 1, 1);
+    }
+
+    updateFrames();
+});
+
+function updateFrames() {
+    for (let i = 1; i <= 3; i++) {
+        const frame = document.getElementById(`frame${i}`);
+        if (i === currentFrame) {
+            frame.style.opacity = 1;
+            frame.style.transform = 'translateX(0)';
+        } else {
+            frame.style.opacity = 0;
+            frame.style.transform = `translateX(${100 * (i - currentFrame)}%)`;
+        }
+    }
 }
 
-.frame {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    transition: opacity 0.5s, transform 0.5s;
-    opacity: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 2em;
-}
 
-#frame1 {
-    background-color: #FF5733;
-    transform: translateX(0);
-}
 
-#frame2 {
-    background-color: #3498DB;
-    transform: translateX(100%);
-}
 
-#frame3 {
-    background-color: #27AE60;
-    transform: translateX(200%);
-}
